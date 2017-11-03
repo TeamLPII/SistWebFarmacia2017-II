0b63a8035260f3546440cb302ada30afd198c2fc
Nielsen Puma <nielhpuma@gmail.com>
2017-11-02 23:34:34 -0500

    Creacion de los ficheros de conexión a la BD según DAO
    Se crean los ficheros para la parte de la implementacion de la BD MYSQL

---
+++ ProyFarmaciaWeb/src/utils/MysqlDBConexion.java
@@ -0,0 +1,35 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MysqlDBConexion {
+	public static Connection getConexion() {
+		Connection cn = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			cn = DriverManager.getConnection("jdbc:mysql://localhost/bd_farmacia2017", "root", "mysql");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return cn;
+	}
+	public static void cerrarConexion(Connection cn, PreparedStatement pstm, ResultSet rs){
+		try {
+			if(rs != null){
+				rs.close();
+			}
+			if(pstm != null){
+				pstm.close();
+			}
+			if(cn != null){
+				cn.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("Error al cerrar la base de datos.\n"+e);
+		}
+	}
+}
